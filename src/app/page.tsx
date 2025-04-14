a953225236fbfd0b8e108ef2e6873bed8ade9754
Anttoam <ogagodev@gmail.com>
2025-04-14 14:06:02 +0900

    :sparkles: サンプルのフォームを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,51 @@
+'use client'
 import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'ユーザーネームは最低でも２文字以上入力してください'
+  })
+})
 
 export default function Home() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema), 
+    defaultValues: {
+      username: '', 
+    }
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
   return (
-    <div>
-      <h1>Hello Nextjs</h1>
-      <Button>Click me</Button>
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField 
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>ユーザーネーム</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field}/>
+              </FormControl>
+              <FormDescription>
+                ２文字以上を入力してください
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">送信</Button>
+      </form>
+    </Form>
 
   );
 }
